35e57c8b38a8294f668862c063e00942a6b02e3e
Harout Mardirossian <harmardir@gmail.com>
2024-07-31 12:15:29 +0300

    Adding dist folder for the 06 edit

---
+++ dist/learning-header/BurgerMenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+var BurgerMenu = function BurgerMenu(_ref) {
+  var onClick = _ref.onClick;
+  return /*#__PURE__*/React.createElement("div", {
+    className: "burger-menu",
+    onClick: onClick
+  }, "\u2630 ");
+};
+export default BurgerMenu;
+//# sourceMappingURL=BurgerMenu.js.map
